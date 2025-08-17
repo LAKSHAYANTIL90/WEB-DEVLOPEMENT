045b0d50a6d3d959cdb1e67b0b4b5e945121beab
Lakshay Antil <Lakshayantil90@gmail.com>
2025-08-17 19:28:47 +0530

    Add files via upload

---
+++ JAVA SCRIPT/new.js
@@ -0,0 +1,3 @@
+console.log("Hey I am a console");
+console.log("Hey I am second running code....");
+console.log("Hey I am another running code..... ");
---
+++ JAVA SCRIPT/script.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+console.log("Hey I am a console");
+
+var a = prompt("Enter Your Number");
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer");
+
+if(isTrue){
+    console.log("Computer is blasting")
+}
+
+else{
+    console.log("Computer is not blasting");
+}
+
+console.log("Your Number is" + a);

25c82b09513229f199ad4b0ac6302a57e57e14ab
Lakshay Antil <Lakshayantil90@gmail.com>
2025-08-23 13:17:50 +0530

    Add files via upload

---
+++ JAVA SCRIPT/Event Bubbling/script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("btn")
+button.addEventListener("click", ()=>{
+    document.querySelector(".box").innerHTML = "<b> I was clicked </b>";
+})
+button.addEventListener("contextmenu", ()=>{
+    alert("Dont hack us by right click please!")
+})
+document.addEventListener("keydown", (e)=>{
+    console.log(e, e.key, e.keyCode)
+})
